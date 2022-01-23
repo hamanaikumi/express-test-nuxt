c3bc86b663c45921f173e7e5813ae900f5167f11
jurasora1010 <ikumi.hamana@rakus-partners.co.jp>
2022-01-23 16:45:16 +0900

    タスク表示画面を作成

--- pages/main/_id.vue
+++ pages/main/_id.vue
@@ -1,7 +1,50 @@
 <template>
-  <div>
-    {{ showTasks }}
-  </div>
+  <v-container>
+    <v-row>
+      <v-col
+        v-for="(category, i) of showTasks"
+        :key="i"
+        class="d-flex"
+        cols="12"
+        sm="6"
+        md="6"
+        lg="4"
+      >
+        <v-card class="ml-8 mt-4 pa-2">
+          <!-- Category -->
+          <v-row class="align-center mx-0 d-flex justify-space-between">
+            <v-card-title>
+              {{ category.categoryName }}
+            </v-card-title>
+            <span>
+              <!-- DeleteCategory -->
+            </span>
+          </v-row>
+          <v-divider class="mx-4 mb-2"></v-divider>
+          <v-card-text
+            v-for="(task, j) of category.task"
+            :key="j"
+            class="py-0 text--primary text-body-1"
+          >
+            <v-row class="align-center">
+              <v-col cols="1" class="mr-2">
+                <input type="checkbox" />
+              </v-col>
+              <span v-show="task.taskCompleted === '0'">
+                {{ task.taskContent }}
+              </span>
+              <span v-show="task.taskCompleted !== '0'" class="completed">
+                {{ task.taskContent }}
+              </span>
+              <span>
+                <!-- Add Tasks -->
+              </span>
+            </v-row>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -18,6 +61,9 @@ export default {
     this.getTasks()
   },
   methods: {
+    /**
+     * ユーザーIDに一致するタスク一覧を表示する.
+     */
     async getTasks() {
       // 初期化
       this.showTasks = []
